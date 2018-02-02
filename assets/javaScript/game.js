db791ec4e1204a82dc1bce87af555b9699acb3c6
Christian Lopes <christianlopes@Christian-Lopess-iMac-2.local>
2018-02-02 11:38:30 -0800

    fixing counters and resets

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,51 +1,51 @@
 // Alphabet array.
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Creating variables to hold the number of wins, losses, guesses left, and letters guessed. 
 var wins = 0
 var losses = 0
-var guessLeft = 50
+var guessLeft = 10
+var wrongGuess = 0
 var guessed = []
+var computerChoice =""
 
+
+
+// computer chooses a letter from the computerChoices array. This is the Computer's guess.
+var computerChoice = letters[Math.floor(Math.random() * letters.length)]
+console.log(computerChoice)
 
 // This function is run whenever the user presses a key.
-document.onkeyup = function (event){ 
+document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key
-    // console.log(userGuess)
-
-    // Randomly chooses a choice from the computerChoices array. This is the Computer's guess.
-    var computerThink = computerChoices[Math.floor(Math.random() * computerChoices.length)]
-    // console.log(computerThink)
-
-    //insert for loop here that runs statements while guesses left >0. Countdown. 
+    console.log(userGuess)
 
 
     // This logic determines the outcome of the game (win/loss), and adjusts the appropriate number. It also incorporates a backwards counter for the amount of tries left. 
 
-    // for (i = 0; i < computerChoices.length; i++){
+
+    if (userGuess === computerChoice) {
+        wins++
+        alert("You guessed right. Nice work!")
         
-        if (userGuess === computerThink) {
-            wins++
-            guessLeft--
-            alert("You guessed right. Nice work!")
-        }
+    }
 
-        else if(guessLeft===0){
-            alert("Game Over! Click Refresh to play again.")
-        }
-    
+    if (userGuess != computerChoice) {
+        wrongGuess++
+        guessLeft--
+        guessed.push(userGuess)
+    }
 
-        else {
-            losses++
-            guessLeft--
-            guessed.push(userGuess)
-            console.log(guessed)
-        }
-    
+    if (guessLeft === 0) {
+        losses++
+        alert("Game Over! ")
+        guessLeft=10
+        guessed=[]
+    }
 
-    // html for the scoreboard
+    // html for scores
     var html =
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
@@ -55,5 +55,5 @@ document.onkeyup = function (event){
     // Set the inner HTML contents of the #game div to our html string
     document.querySelector("#game").innerHTML = html;
 
-    
+
 }
